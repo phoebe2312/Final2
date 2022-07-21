c57177a5a46d362c778f88f32fcf3abfec6ddd81
Phoebe Chen <kexing42@gmail.com>
2022-07-21 00:09:56 -0400

    Added about, to do page, and UI view controller file for each new view controller

---
+++ CalendarExampleTutorial/ViewControllerAbout.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerAbout.swift
+//  CalendarExampleTutorial
+//
+//  Created by Phoebe Chen on 7/21/22.
+//
+
+import UIKit
+
+class ViewControllerAbout: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
