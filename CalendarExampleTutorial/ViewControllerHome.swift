4957eacefc4fde85585f780d5a5368376d932991
Phoebe Chen <kexing42@gmail.com>
2022-07-20 23:58:25 -0400

    Added about us page, todo page, and added UI view controller file for each new view controller

---
+++ CalendarExampleTutorial/ViewControllerHome.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerHome.swift
+//  CalendarExampleTutorial
+//
+//  Created by Phoebe Chen on 7/20/22.
+//
+
+import UIKit
+
+class ViewControllerHome: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
